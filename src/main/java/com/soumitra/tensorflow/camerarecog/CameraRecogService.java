e81a85ff651e55918b4755a3900413d8907c157b
Soumitra Mohakud <soumitra.mohakud@gfs.com>
2017-12-06 16:23:22 +0530

    Re-enabling CORS

--- src/main/java/com/soumitra/tensorflow/camerarecog/CameraRecogService.java
+++ src/main/java/com/soumitra/tensorflow/camerarecog/CameraRecogService.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Soumitra Mohakud This uses an already trained model to identify
  *         images.
  */
+@CrossOrigin
 @RestController
 public class CameraRecogService {
 
